7f514f01fdd07819f62cb21e283c9113e7c5734f
Timon Käch <78795905+CyberTimon@users.noreply.github.com>
2026-03-01 21:05:42 +0100

    Change no-unused-vars rule from error to warn

--- eslint.config.js
+++ eslint.config.js
@@ -50,7 +50,7 @@ module.exports = [
     rules: {
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': [
-        'error',
+        'warn',
         { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrorsIgnorePattern: '^_' },
       ],
       // '@typescript-eslint/no-explicit-any': 'off',
